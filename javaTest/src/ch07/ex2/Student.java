949da2ad921fcd6fb2e1cbaa283a950909d5881a
Back Hyeyeon <skwir21@gmail.com>
2023-09-27 11:35:31 +0900

    [7장 실습 내용수정 ]

--- javaTest/src/ch07/ex2/Student.java
+++ javaTest/src/ch07/ex2/Student.java
@@ -3,71 +3,64 @@ package ch07.ex2;
 import java.util.Objects;
 
 public class Student {
-	    private String name;
-	    private String major;
-	    private int id;
-	    private double gpa;
+	private String name;
+	private String major;
+	private int id;
+	private double gpa;
 
-	    public Student() {
-	        this(null, null, 0, 0.0);
-	    }
-
-	    public Student(String name, String major, int id, double gpa) {
-	        super();
-	        this.name = name;
-	        this.major = major;
-	        this.id = id;
-	        this.gpa = gpa;
-	    }
-	    public Student(String name, int id) {
-	    	this(name,null,id,0.0);
-	    }
-
-	    public String getName() {
-	        return name;
-	    }
-
-	    public String getMajor() {
-	        return major;
-	    }
-
-	    public int getId() {
-	        return id;
-	    }
-
-	    public double getGpa() {
-	        return gpa;
-	    }
-
-	    @Override
-	    public String toString() {
-	        return "Student [name=" + name + ", major=" + major + ", id=" + id + ", gpa=" + gpa + "]";
-	    }
-
-	    @Override
-	    public int hashCode() {
-	        return Objects.hash(this.name,this.id);
-	    }
-
-	    @Override
-	    public boolean equals(Object obj) {
-	        Student stu = null;
-	        if(!(obj instanceof Student)) {
-	            return false;
-	        }
-	        stu = (Student)obj;
-	        if(this.name.equals(stu.name)&&this.id==stu.id) {
-	            return true;
-	        }
-	        return false;
-	    }
-
+	public Student() {
+		this(null, null, 0, 0.0);
 	}
 
+	public Student(String name, String major, int id, double gpa) {
+		super();
+		this.name = name;
+		this.major = major;
+		this.id = id;
+		this.gpa = gpa;
+	}
 
+	public Student(String name, int id) {
+		this(name, null, id, 0.0);
+	}
 
+	public String getName() {
+		return name;
+	}
 
+	public String getMajor() {
+		return major;
+	}
 
+	public int getId() {
+		return id;
+	}
 
+	public double getGpa() {
+		return gpa;
+	}
 
-	
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", major=" + major + ", id=" + id + ", gpa=" + gpa + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.name, this.id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Student stu = null;
+		if (!(obj instanceof Student)) {
+			return false;
+		}
+		stu = (Student) obj;
+		if (this.name.equals(stu.name) && this.id == stu.id) {
+			return true;
+		}
+		return false;
+	}
+
+}
--- javaTest/src/ch07/ex2/StudentMain.java
+++ javaTest/src/ch07/ex2/StudentMain.java
@@ -1,41 +1,42 @@
 package ch07.ex2;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
 public class StudentMain {
-    public static Scanner scan = new Scanner(System.in);
+	public static Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        ArrayList<Student> al = new ArrayList<Student>();
+	public static void main(String[] args) {
+		ArrayList<Student> al = new ArrayList<Student>();
 
-        System.out.printf("4명의 학생,이름,학과,학번,학점평균을 입력하시오\n");
-        for (int i = 0; i < 4; i++) {
-            System.out.printf(">>");
-            String[] data = scan.nextLine().trim().split(",");
-            al.add(new Student(data[0], data[1], Integer.parseInt(data[2]), Double.parseDouble(data[3])));
-        }
+		System.out.printf("4명의 학생,이름,학과,학번,학점평균을 입력하시오\n");
+		for (int i = 0; i < 4; i++) {
+			System.out.printf(">>");
+			String[] data = scan.nextLine().trim().split(",");
+			al.add(new Student(data[0], data[1], Integer.parseInt(data[2]), Double.parseDouble(data[3])));
+		}
 
-        System.out.println("===========정보출력=============");
-        Iterator<Student> ir = al.iterator();
-        while(ir.hasNext()) {
-            System.out.printf("%s \n",ir.next());
-        }
+		System.out.println("===========정보출력=============");
+		Iterator<Student> ir = al.iterator();
+		while (ir.hasNext()) {
+			System.out.printf("%s \n", ir.next());
+		}
 
-        System.out.println("===========정보출력=============");
+		System.out.println("===========정보출력=============");
 
-        while(true) {
-            System.out.printf("학생이름>>");
-            String name = scan.next().trim();
-            System.out.printf("학생번호>>");
-            int id = Integer.parseInt(scan.next().trim());
-            Student findStu = new Student(name, id);
-            boolean isfind = al.contains(findStu);
-            if(isfind) {
-            	System.out.printf("%s\n",al.get(al.indexOf(findStu)));
-            }else {
-            	System.out.printf("찾지 못했습니다.\n");
-            }
-        }
-    }
+		while (true) {
+			System.out.printf("학생이름>>");
+			String name = scan.next().trim();
+			System.out.printf("학생번호>>");
+			int id = Integer.parseInt(scan.next().trim());
+			Student findStu = new Student(name, id);
+			boolean isfind = al.contains(findStu);
+			if (isfind) {
+				System.out.printf("%s\n", al.get(al.indexOf(findStu)));
+			} else {
+				System.out.printf("찾지 못했습니다.\n");
+			}
+		}
+	}
 }
