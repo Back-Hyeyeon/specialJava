52e825bbe5c3c760b908ac2c051df0a7ba12a37c
Back Hyeyeon <skwir21@gmail.com>
2023-09-27 13:08:10 +0900

    [a scholarship program Main2 수정] while을 for 문으로 수정

--- javaTest/src/ch07/ex3/Main2.java
+++ javaTest/src/ch07/ex3/Main2.java
@@ -9,28 +9,24 @@ public class Main2 {
 	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		Student[] student = new Student[5];
-		HashMap<String, Double> hm = new HashMap<>();
+		HashMap<String, Student> hm = new HashMap<>();
 		
 		System.out.println("미래장학금관리시스템입니다.");
 		for(int i=0; i<student.length; i++) {
 			System.out.print("이름과 학점 >> ");
 			String name = sc.next();
 			double av = sc.nextDouble();
-			hm.put(name, av);
+			hm.put(name, new Student(name, av));
 		}
 		
 		System.out.print("장학생 선발 학점 기준 입력>> ");
 		double standard = sc.nextDouble();
 		
 		Set<String> keys = hm.keySet();
-		Iterator<String> it = keys.iterator();
-		
-		System.out.print("장학생 명단 : ");
-		while(it.hasNext()) {
-			String stuName = it.next();
-			double score = hm.get(stuName);
-			if(score >= standard) {
-				System.out.print(stuName+" ");
+		for(String data:keys) {
+			Student stu = hm.get(data);
+			if(stu.getAv() >= standard) {
+				System.out.printf("%s \n",stu.getName());
 			}
 		}
 	}
